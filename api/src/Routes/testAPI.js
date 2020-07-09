29c09cb227ed17f693a74da1002109d1e64ccbbf
soumyanayak95 <soumyanayak95@gmail.com>
2020-07-09 13:12:58 +0530

    used is-git-url lib to check the github url and send 500 error response

--- api/src/Routes/testAPI.js
+++ api/src/Routes/testAPI.js
@@ -1,15 +1,10 @@
 var express = require("express");
 var router = express.Router();
 var bodyParser = require("body-parser");
-var jsonParser = bodyParser.json();
-var shell = require("shelljs");
-const exec = require("child_process").exec;
+var isGitUrl = require('is-git-url');
 const spawn = require("child_process").spawn;
 
 var app = express();
-
-// parse application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: false }));
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(bodyParser.json());
@@ -22,51 +17,25 @@ router.get("/test", function (req, res, next) {
   res.send({ text: "testAPI/test method" });
 });
 
-//bash script execute
-const X = "this is a param";
-
-// router.get("/bash", function (req, res, next) {
-//   exec("./bash.sh", (err, stdout, stderr) => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     console.log(stdout);
-//   });
-
-//   res.send("Bash script executed here");
-// });
 
 router.post("/", urlencodedParser, function (req, res) {
-  console.log(req.body.url + " <- this is request body!!!!!!!");
-
-  const u = req.body.url;
-
-  res.send("response string from node");
-
-  // shell.echo(url);
-
-  // const ls = exec("ls", ["-lh", "/usr"]);
-
-  // ls.stdout.on("data", (data) => {
-  //   console.log(`stdout: ${data}`);
-  // });
-  // ls.on("close", (code) => {
-  //   console.log(`child process exited with code ${code}`);
-  // });
-
-  // shell.exec("cd ../");
-  // shell.exec("ls");
-
-  exec("bash.sh " + `'${u}'`, (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(stdout);
-  });
-
-  //res.send("Bash script executed here");
+  const url = req.body.url;
+  console.log(url);
+  if ( !isGitUrl(url) ){
+    console.log("Invalid Git url!!!")
+    res.status(500).send("Invalid Git url");
+  }
+  else{
+    const ls = spawn("git", ['clone', url]);
+    ls.stdout.on("data", (data) => console.log("data", data.toString('utf-8')));
+    ls.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+    });
+    ls.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+    res.send(url);
+  }
 });
 
 module.exports = router;

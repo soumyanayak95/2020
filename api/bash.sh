bc4b07e325384b536464413899a9fce4c135c8e7
soumyanayak95 <soumyanayak95@gmail.com>
2020-07-13 22:51:36 +0530

    bash script not running issue fixed

--- api/bash.sh
+++ api/bash.sh
@@ -1,11 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # echo $1
 # cd into the cloned repo
 
 cd ./$(basename $1 .git)
 ls
 
+# ssh into aws ec2
+# ec2 having docker
+
 docker build -t srn/nodeapp .
-docker run -p 3000:8080 -d  srn/nodeapp
+# docker run -p 3000:8080 -d  srn/nodeapp
 
 $SHELL
--- api/src/Routes/deploy.js
+++ api/src/Routes/deploy.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var isGitUrl = require('is-git-url');
 const spawn = require("child_process").spawn;
 var shell = require("shelljs");
-// const { exec } = require("child_process");
+const { exec } = require("child_process");
 
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -15,7 +15,6 @@ router.get("/", function (req, res, next) {
   res.send("deploy your app here");
 });
 
-
 router.post("/", urlencodedParser, function (req, res) {
   const url = req.body.url;
   console.log(url);
@@ -23,30 +22,42 @@ router.post("/", urlencodedParser, function (req, res) {
   try{
     if ( !isGitUrl(url) ){
       console.log("Invalid Git url!!!")
-      throw new Error("Invalid Git url");
+      throw new Error("Invalid Git url inside the throw");
     }else{
-      const ls = spawn("git", ['clone', url]);
-      ls.stdout.on("data", (data) => console.log("data", data.toString('utf-8')));
-      ls.stderr.on('data', (data) => {
-          console.error(`stderr: ${data}`)
-      });
-      ls.on('close', (code) => {
-          console.log(`child process exited with code ${code}`);
-      });
+      // const ls = spawn("git", ['clone', url]);
+      // ls.stdout.on("data", (data) => console.log("data", data.toString('utf-8')));
+      // ls.stderr.on('data', (data) => {
+      //     console.error(`stderr: ${data}`)
+      // });
+      // ls.on('close', (code) => {
+      //     console.log(`child process exited with code ${code}`);
+      // });
+
+      if (shell.exec(` git clone ${url} `).code !== 0) {
+        shell.echo('Error: Git commit failed');
+        shell.exit(1);
+      }
+      else{
+        console.log ("succesfully cloned!!!")
+      }
 
       // echo url in bash script
       
-      // exec("bash.sh " + `'${url}'` , (err, stdout, stderr) => {
-      //   if (err){
-      //     console.error(err);
-      //   }
-      //   console.log(stdout)
-      // });
+      console.log("cloning done and now to run the bash script")
+
+      shell.exec('./bash.sh ' + `${url}` , (err, stdout, stderr) => {
+        if (err){
+          console.log("some error inside the bash script execution")
+          console.log(err)
+          console.error(err);
+        }
+        console.log(stdout)
+      });
 
       res.send(url);
     }
   }catch(e){
-    res.status(500).send("Invalid Git url");
+    res.status(500).send("Invalid Git url inside the catch");
   }
 
 });

d396726ef78cac4399fb31001fa1536ad1846982
soumyanayak95 <soumyanayak95@gmail.com>
2020-07-13 08:09:25 +0530

    dockerfile changed and code refactored

--- api/bash.sh
+++ api/bash.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-echo $1
-
+# echo $1
 # cd into the cloned repo
-# cd ./$(basename $1 .git)
-# ls
+
+cd ./$(basename $1 .git)
+ls
+
+docker build -t srn/nodeapp .
+docker run -p 3000:8080 -d  srn/nodeapp
 
 $SHELL
--- api/src/Routes/deploy.js
+++ api/src/Routes/deploy.js
@@ -4,43 +4,44 @@ var bodyParser = require("body-parser");
 var isGitUrl = require('is-git-url');
 const spawn = require("child_process").spawn;
 var shell = require("shelljs");
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(bodyParser.json());
 
-router.get("/test", function (req, res, next) {
-  res.send({ text: "testAPI/test method" });
+router.get("/", function (req, res, next) {
+  res.send("deploy your app here");
 });
 
 
 router.post("/", urlencodedParser, function (req, res) {
   const url = req.body.url;
   console.log(url);
-
+  
   try{
     if ( !isGitUrl(url) ){
       console.log("Invalid Git url!!!")
       throw new Error("Invalid Git url");
     }else{
-      // const ls = spawn("git", ['clone', url]);
-      // ls.stdout.on("data", (data) => console.log("data", data.toString('utf-8')));
-      // ls.stderr.on('data', (data) => {
-      //     console.error(`stderr: ${data}`)
-      // });
-      // ls.on('close', (code) => {
-      //     console.log(`child process exited with code ${code}`);
-      // });
+      const ls = spawn("git", ['clone', url]);
+      ls.stdout.on("data", (data) => console.log("data", data.toString('utf-8')));
+      ls.stderr.on('data', (data) => {
+          console.error(`stderr: ${data}`)
+      });
+      ls.on('close', (code) => {
+          console.log(`child process exited with code ${code}`);
+      });
 
       // echo url in bash script
-      exec("bash.sh " + `'${url}'` , (err, stdout, stderr) => {
-        if (err){
-          console.error(err);
-        }
-        console.log(stdout)
-      });
+      
+      // exec("bash.sh " + `'${url}'` , (err, stdout, stderr) => {
+      //   if (err){
+      //     console.error(err);
+      //   }
+      //   console.log(stdout)
+      // });
 
       res.send(url);
     }
